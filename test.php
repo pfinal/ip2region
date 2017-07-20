da8b25f9323f7e4863b13735010dcf9c9eb380d7
ethan <nicesoft333@qq.com>
2017-07-20 12:03:54 +0800

    doc

--- test.php
+++ test.php
@@ -6,10 +6,10 @@ use PFinal\Ip2Region\Ip2Region;
 
 $ip2region = new Ip2Region();
 
-$ip = '180.173.2.175';
+$ip = '202.96.128.86';
 
 $info = $ip2region->btreeSearch($ip);
 
 var_dump($info);
 
-//array(2) { ["city_id"]=> int(995) ["region"]=> string(40) "中国|华东|上海市|上海市|电信" }
+//array(2) { ["city_id"]=> int(2140) ["region"]=> string(40) "中国|华南|广东省|广州市|电信" }
